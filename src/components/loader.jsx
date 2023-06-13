fa9e8532e571726ac1c6b95f4bbc863d2490dfd8
EilonV <73593619+EilonV@users.noreply.github.com>
2023-06-13 19:51:57 +0300

    added loader base (just need to animate)

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,21 +1,22 @@
 import logo from '../assets/pics/logo.svg'
+// import balls from '../assets/pics/balls.svg'
 
 export const Loader = () => {
     return <section className="loader">
-        <img src={logo} alt="winwin logo" />
+        <img className='logo' src={logo} alt="winwin logo" />
+        {/* <img className='balls' src={balls} alt="pagination indicator" /> */}
+
         <div className='balls'>
-            <div className='ball'>
-                <div className='ball-connector'></div>
+            <div className='ball-connector-wrapper'>
+                <div className='ball-connector ball-connector1'></div>
+                <div className='ball-connector ball-connector2'></div>
             </div>
-            <div className='ball'>
-                <div className='ball-connector'></div>
-            </div>
-            <div className='ball'>
-                <div className='ball-connector'></div>
-            </div>
+            <div className='ball ball1'> </div>
+            <div className='ball ball2'> </div>
+            <div className='ball ball3'> </div>
         </div>
-        <p>להרוויח מהכישרון שלך יותר</p>
-        {/* <p>להגיע אל יותר לקוחות</p> */}
-        {/* <p>הארנק נשאר בכיס</p> */}
+        <p className='p1'>להרוויח מהכישרון שלך יותר</p>
+        <p className='p2'>להגיע אל יותר לקוחות</p>
+        <p className='p3'>הארנק נשאר בכיס</p>
     </section>
 }
